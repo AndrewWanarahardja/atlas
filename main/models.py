6f3f36d281f4bef08b28e59c02b7e293e6535fde
AndrewWanarahardja <andrew.wanarahardja@gmail.com>
2025-09-21 10:03:28 +0700

    tugas 4 code

--- main/models.py
+++ main/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Products(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     CATEGORY_CHOICES = [
         ('update', 'Update'),
         ('regional', 'Regional'),
